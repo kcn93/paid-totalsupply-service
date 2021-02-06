dc57bd7528eb2837952db21827d9126337e791ef
alfredolopez80 <alfredolopez80@gmail.com>
2021-02-05 20:13:06 -0400

    FIX: critical Security Issue with Jade Template

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
